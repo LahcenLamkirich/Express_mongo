8fef6dda84c44d235543df7768be97342a91bb35
LahcenLamkirich <lahcen.lamkirich@gmail.com>
2022-01-26 19:24:49 +0100

    commit

--- routes/movies.js
+++ routes/movies.js
@@ -3,18 +3,25 @@ const router = express.Router();
 const {PrismaClient} = require('@prisma/client')
 const prisma = new PrismaClient()
  
-router.get('/', async function(req, res, next) {
-    const movies = await prisma.movies.findMany({take: 10})
-  res.send({
-      data: movies,
-    //   pagination : {
-    //       count : total, // Total des enregistrements
-    //       take: limit,   // Nombre d'éléments sélectionnés
-    //       skip: offset   // Décalage à partir duquel on prend les  données
-    // }
-  });
 
+ router.get('/', async function (req, res, next) {
+    const take = parseInt(req.query.take) || 10;
+    const skip = parseInt(req.query.skip) || 0;
+    const total= await prisma.movies.count();
+
+    const movies = await prisma.movies.findMany({
+        take: take,
+        skip: skip,
+    })
+
+    res.send({
+        data: movies,
+        pagination: {
+            count: total,
+            skip: skip,
+            take: take,
+        }
+    });
 
 });
- 
 module.exports = router;
